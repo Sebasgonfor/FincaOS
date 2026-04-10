07dc8674b90d86be85eb1156ce5d1328c478dd45
Alanis Zambrano <AlanisZambranoTtech@gmail.com>
2026-04-10 00:30:33 -0500

    Add Cross-Origin-Opener-Policy header to next.config.js

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
     ignoreDuringBuilds: true,
   },
   images: { unoptimized: true },
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
